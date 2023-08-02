2931f6e23d76505b428a23c011871c436237878c
Christopher Garcia <chrisga@sas.upenn.edu>
2023-08-02 15:22:07 -0500

    added unique user check functionality so only unique usernames can connect to socket

--- app/api/join_game_page.py
+++ app/api/join_game_page.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, request, session, url_for, redirect
-
+from app.socket_events import usernames
 
 # join_game blueprint for landing page of app
 # should maintain logic over joining quiz queue and redirection to game_room
@@ -7,9 +7,13 @@ from flask import Blueprint, render_template, request, session, url_for, redirec
 join_game_page = Blueprint('join_game_page', __name__)
 
 
+def handle_check_user_exists(username):
+    if username in usernames:
+        return render_template('game/join_game_page.html', error='Username already exists, please list another name')
+
+
 @join_game_page.route('/', methods=['POST', 'GET'])
 def join_view():
-
     # receives POST request from form listed in join_game_page.html
     if request.method == 'POST':
 
@@ -17,7 +21,8 @@ def join_view():
         # else no name, display current template and send error to template
         name = request.form.get('name')
         if name:
-            print(name)
+            if name.lower() in usernames:
+                return render_template('game/join_game_page.html', error='Username already exists, please list another name')
         else:
             return render_template('game/join_game_page.html', error='Please provide a name')
 
