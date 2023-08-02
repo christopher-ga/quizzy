3759596de7cf8c4e7b20fe58278fcd45a5fae44b
Christopher Garcia <chrisga@sas.upenn.edu>
2023-08-02 14:51:42 -0500

    added add/delete user handlers, implemented users socket live update

--- app/socket_events.py
+++ app/socket_events.py
@@ -1,7 +1,33 @@
 from flask_socketio import send
-from flask import request
+from flask import request, session
 
 users = []
+usernames = set()
+
+
+def handle_add_user(name, socket_id):
+    global users, usernames  # declare users and usernames as global
+
+    user = {
+        "username": name,
+        "id": socket_id
+    }
+
+    usernames.add(user['username'].lower())
+
+    users.append(user)
+
+
+def handle_delete_user(socket_id):
+    target_user = ''
+
+    for i, user in enumerate(users):
+        if user['id'] == socket_id:
+            target_user = users[i]
+            del users[i]
+
+    usernames.discard(target_user['username'])
+    return target_user
 
 
 def define_socket_events(socketio):
@@ -13,15 +39,17 @@ def define_socket_events(socketio):
     @socketio.on('connect')
     def test_connect():
         print('CLIENT CONNECTED')
-        users.append(request.sid)
+        handle_add_user(session['name'], request.sid)
         print(users)
+        print(usernames)
 
     # when a user disconnects from the socket do the following
     @socketio.on('disconnect')
     def disconnect():
-        print('CLIENT DISCONNECTED')
+        deleted_user = handle_delete_user(request.sid)
+        print(f"{deleted_user['username']} has disconnected")
+        print(users)
+        print(usernames)
 
         # emit a custom event to ALL connected clients along with an object containing a message
-        socketio.emit('user_disconnected', {'message': 'A user has disconnected'})
-
-
+        socketio.emit('user_disconnected', {'message': f"A {deleted_user} has disconnected"})
