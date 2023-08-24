071a205f669f999827bed97be30c2bb442c16be8
Adrian Sharpe <adriansharpe148@gmail.com>
2023-08-23 20:54:00 -0400

    Add separate room logic within join_game and game_room views

--- app/api/game_room_page.py
+++ app/api/game_room_page.py
@@ -1,5 +1,5 @@
-from flask import Blueprint, render_template, request
-from app.socket_events import usernames
+from flask import Blueprint, render_template, request, session, redirect, url_for
+from app.socket_events import rooms
 
 
 
@@ -12,6 +12,11 @@ game_room_page = Blueprint('game_room_page', __name__)
 
 @game_room_page.route('/', methods=['POST', 'GET'])
 def join_view():
-    return render_template('game/game_room_page.html', usernames=usernames)
+    room = session.get("room")
+    name = session.get("name")
+    if room == None or name == None or room not in rooms:
+        return redirect(url_for("join_game_page.join_view"))
+    rooms[room]["usernames"].add(name)
+    return render_template('game/game_room_page.html', usernames=rooms[room]["usernames"], code=room)
 
 
--- app/socket_events.py
+++ app/socket_events.py
@@ -1,5 +1,5 @@
 import eventlet
-from flask_socketio import send, join_room
+from flask_socketio import send, join_room, leave_room
 from flask import request, session
 
 eventlet.monkey_patch()
@@ -7,30 +7,32 @@ eventlet.monkey_patch()
 users = []
 usernames = set()
 
-
-def handle_add_user(name, socket_id):
-    global users, usernames  # declare users and usernames as global
-
-    user = {
-        "username": name,
-        "id": socket_id
-    }
-
-    usernames.add(user['username'].lower())
-
-    users.append(user)
+rooms = {} # dict of room codes containing user data
 
 
-def handle_delete_user(socket_id):
-    target_user = ''
+# def handle_add_user(name, socket_id):
+#     global users, usernames  # declare users and usernames as global
 
-    for i, user in enumerate(users):
-        if user['id'] == socket_id:
-            target_user = users[i]
-            del users[i]
+#     user = {
+#         "username": name,
+#         "id": socket_id
+#     }
 
-    usernames.discard(target_user['username'].lower())
-    return target_user
+#     usernames.add(user['username'].lower())
+
+#     users.append(user)
+
+
+# def handle_delete_user(socket_id):
+#     target_user = ''
+
+#     for i, user in enumerate(users):
+#         if user['id'] == socket_id:
+#             target_user = users[i]
+#             del users[i]
+
+#     usernames.discard(target_user['username'].lower())
+#     return target_user
 
 
 def start_timer(socketio):
@@ -49,10 +51,20 @@ def define_socket_events(socketio):
     # when a user connects to the socket do the following
     @socketio.on('connect')
     def test_connect():
-        print('CLIENT CONNECTED')
-        handle_add_user(session['name'], request.sid)
-        print(users)
-        print(usernames)
+        # print('CLIENT CONNECTED')
+        # handle_add_user(session['name'], request.sid)
+        # print(users)
+        # print(usernames)
+        room = session.get("room")
+        name = session.get("name")
+        if not room or not name:
+            return
+        if room not in rooms:
+            leave_room(room)
+            return
+        join_room(room)
+
+        send({"name": name, "message": "has entered the room"}, to=room)
 
         # when there are two users in the game_room_page, start a timer
         if len(users) == 2:
@@ -62,10 +74,20 @@ def define_socket_events(socketio):
     # when a user disconnects from the socket do the following
     @socketio.on('disconnect')
     def disconnect():
-        deleted_user = handle_delete_user(request.sid)
-        print(f"{deleted_user['username']} has disconnected")
-        print(users)
-        print(usernames)
+        # deleted_user = handle_delete_user(request.sid)
+        # print(f"{deleted_user['username']} has disconnected")
+        # print(users)
+        # print(usernames)
+
+        room = session.get("room")
+        name = session.get("name")
+        leave_room(room)
+
+        if room in rooms and name in rooms[room]["usernames"]:
+            rooms[room]["usernames"].remove(name)
+            if len(rooms[room]["usernames"]) <= 0:
+                del rooms[room]
+        # send({"name": name, "message": "has left the room"}, to=room)
 
         # emit a custom event to ALL connected clients along with an object containing a message
-        socketio.emit('user_disconnected', {'message': f"A {deleted_user} has disconnected"})
+        socketio.emit('user_disconnected', {'message': f"A {name} has disconnected"}, to=room)
