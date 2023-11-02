6cad96d994237a24018e5a47a594851068b07573
celinaczy <celina.czyszczon@gmail.com>
2023-11-02 13:19:00 +0100

    randomize answer order, unescape html characters

--- app/fixtures/quiz_generate.py
+++ app/fixtures/quiz_generate.py
@@ -1,5 +1,7 @@
+# from flask import request
 import requests
-
+import html
+# import gevent.monkey
 # category ids
 # general knowledge  9
 # comp-sci 18
@@ -15,7 +17,7 @@ def generate_quiz(category_id):
         'category': category_id
     }
     response = requests.get("https://opentdb.com/api.php", params=parameters)
-    response.raise_for_status()
+    # response.raise_for_status()
     api_response = response.json()
 
     # Initialize a question ID counter
@@ -28,17 +30,28 @@ def generate_quiz(category_id):
     # Loop through the API response and convert it to QUIZZES format
     for api_question in api_response["results"]:
 
+        #sort to mix the correct answer with incorrect ones
+        options = sorted(api_question["incorrect_answers"] + [api_question["correct_answer"]])
+
+        #find the correct option
+        correct = ""
+        option_id = ["a", "b", "c", "d"]
+        for num, i in enumerate(options):
+            if i == api_question["correct_answer"]:
+                correct = option_id[num]
+        # print(options, api_question["correct_answer"], correct)
+
         # Create the question dictionary
         question = {
             "id": question_id_counter,
-            "question_text": api_question["question"],
+            "question_text": html.unescape(api_question["question"]),
             "choices": [
-                {"id": "a", "choice_text": api_question["incorrect_answers"][0]},
-                {"id": "b", "choice_text": api_question["incorrect_answers"][1]},
-                {"id": "c", "choice_text": api_question["incorrect_answers"][2]},
-                {"id": "d", "choice_text": api_question["correct_answer"]},
+                {"id": "a", "choice_text": html.unescape(options[0])},
+                {"id": "b", "choice_text": html.unescape(options[1])},
+                {"id": "c", "choice_text": html.unescape(options[2])},
+                {"id": "d", "choice_text": html.unescape(options[3])},
             ],
-            "correct": "d",
+            "correct": correct,
         }
 
         # Append the question to the quiz
